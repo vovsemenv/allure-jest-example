85a35aff6e7ede0b6a1edbf20e3248ee32b29181
Vladimir Semenov <vovsemenv@gmail.com>
2023-03-01 18:07:47 +0300

    fix

--- spec/helpers/allure.ts
+++ spec/helpers/allure.ts
@@ -2,10 +2,9 @@ import { JasmineAllureReporter } from "allure-jasmine";
 import { JasmineAllureInterface } from "allure-jasmine/dist/src/JasmineAllureReporter";
 
 const reporter = new JasmineAllureReporter({ resultsDir: "allure-results" });
-jasmine.getEnv().addReporter(reporter);
+(jasmine as any).getEnv().addReporter(reporter);
 // @ts-expect-error
 global.allure = reporter.getInterface();
-
 
 declare global {
   const allure: JasmineAllureInterface;
